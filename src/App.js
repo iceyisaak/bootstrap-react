6aa41c38abc05dae135b07d993314881fa45a90b
Icey Isaak <me@iceyisaak.com>
2020-12-24 15:15:08 +0100

    Testing Bootstrap for Reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Button from 'react-bootstrap/Button';
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Alert,
+  Breadcrumb,
+  Card,
+  Form
+} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+
+          <Form>
+            <Row>
+              <Col md>
+                <Form.Group
+                  controlId="formEmail"
+                >
+                  <Form.Label>
+                    Email Address
+            </Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="example@email.com"
+                  />
+
+                  <Form.Text
+                    className="text-muted"
+                  >
+                    We'll never share your email address
+            </Form.Text>
+                </Form.Group>
+              </Col>
+              <Col md>
+                <Form.Group
+                  controlId="formPassword"
+                >
+                  <Form.Label>
+                    Password
+            </Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                  />
+
+                </Form.Group>
+              </Col>
+
+            </Row>
+            <Button
+              variant="secondary"
+              type="submit"
+            >
+              Login
+              </Button>
+          </Form>
+          <Card
+            className="mb-3"
+            style={{
+              color: "#000"
+            }}
+          >
+            <Card.Img
+              src="https://picsum.photos/200/50"
+            />
+            <Card.Body>
+              <Card.Title>
+                Card Example
+              </Card.Title>
+              <Card.Text>
+                This is an example of React Bootstrap Cards
+              </Card.Text>
+              <Button
+                variant="primary"
+              >
+                read more
+              </Button>
+            </Card.Body>
+          </Card>
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              Test
+        </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              Test 2
+        </Breadcrumb.Item>
+            <Breadcrumb.Item active>
+              Test 3
+        </Breadcrumb.Item>
+          </Breadcrumb>
+          <Alert
+            variant="success"
+          >
+            This is a button
+        </Alert>
+          <Button>
+            Test Button
+        </Button>
+        </Container>
       </header>
-    </div>
+    </div >
   );
 }
 
